1151afd48b5652fb7588cc73a5c22b2b98b4d1c3
KEVIN (W) WEPONDI <104903442+kevi-t@users.noreply.github.com>
2024-11-18 12:30:44 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,15 @@
 // Scripts
 // 
 
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -30,5 +39,39 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+
+    // Toggle sidebar visibility on smaller screens
+    const sideNavToggle = document.getElementById('sidebar-toggler'); // Button to toggle sidebar visibility
+    if (sideNavToggle) {
+        sideNavToggle.addEventListener('click', () => {
+            sideNav.classList.toggle('show'); // Add or remove 'show' class to toggle sidebar visibility
+        });
+    }
+
+});
+
+// window.addEventListener('DOMContentLoaded', event => {
+
+//     // Activate Bootstrap scrollspy on the main nav element
+//     const sideNav = document.body.querySelector('#sideNav');
+//     if (sideNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#sideNav',
+//             offset: 74,
+//         });
+//     };
+
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
 
 });
